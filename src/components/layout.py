759103abaf53f2291a5978169fe53bbaf342a7eb
JesperSqv <jesper.sundqvist@netikka.fi>
2024-10-28 14:25:36 +0200

    Added comma

--- src/components/layout.py
+++ src/components/layout.py
@@ -2,7 +2,7 @@ from Dash import Dash, html
 
 def create_layout(app: Dash) -> html.div:
     return html.Div(
-        className="app-div"
+        className="app-div",
         children=[
             html.H1(app.title),
             html.Hr,
