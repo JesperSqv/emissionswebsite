651dc13fc33215eb31ad62c720e50a3fb2172b17
JesperSqv <jesper.sundqvist@netikka.fi>
2024-10-29 15:27:51 +0200

    Added the line chart to the layout

--- src/components/layout.py
+++ src/components/layout.py
@@ -1,10 +1,15 @@
 from dash import Dash, html
 
+import pandas as pd
+
+from . import line_chart
+
 def create_layout(app: Dash) -> html.Div:
     return html.Div(
         className="app-div",
         children=[
             html.H1(app.title),
             html.Hr(),
-        ]
+            line_chart.render(app),
+        ],
     )
