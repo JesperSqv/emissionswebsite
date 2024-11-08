c3f30c33ddefb578e719daaa0c73cea5a253f093
JesperSqv <jesper.sundqvist@netikka.fi>
2024-11-08 17:52:44 +0200

    Made it run 0.0.0.0

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import os
 from dash import Dash, html
 from dash_bootstrap_components.themes import BOOTSTRAP
 
@@ -7,8 +8,12 @@ def main() -> None:
     app = Dash(external_stylesheets=[BOOTSTRAP])
     app.title = "Emission data"
     app.layout = create_layout(app)
-    app.run()
-
+    
+    # Get the port from environment variables, defaulting to 8050 if not set
+    port = int(os.environ.get("PORT", 8050))
+    
+    # Run the app on host 0.0.0.0 to make it accessible externally
+    app.run_server(host="0.0.0.0", port=port)
 
 if __name__ == "__main__":
     main()
